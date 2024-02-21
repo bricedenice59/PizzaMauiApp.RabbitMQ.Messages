daf7bfdcea172d81032583fdff82c35593925146
Brice Grenard <brice.grenard@gmail.com>
2024-02-21 10:41:54 +0100

    Fix previous commit (csproj was not there). Remove a property and its reference on another DTO that is not used.

--- IOrderMessage.cs
+++ IOrderMessage.cs
@@ -1,5 +1,3 @@
-using PizzaMauiApp.API.Dtos.Enums;
-
 namespace PizzaMauiApp.RabbitMq.Messages;
 
 public interface IOrderMessage
@@ -7,5 +5,4 @@ public interface IOrderMessage
     public Guid OrderId { get; set; }
     public Guid UserId { get; set; }
     public List<IOrderItem> Items { get; set; }
-    public OrderStatus Status { get; set; }
 }
