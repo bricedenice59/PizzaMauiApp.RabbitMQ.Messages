3d502fb742b537703f4c5eafc13935b8f96f53e0
Brice Grenard <brice.grenard@gmail.com>
2024-02-21 10:34:51 +0100

    Add shared messages for my current implementation

---
+++ IKitchenOrderAccepted.cs
@@ -0,0 +1,9 @@
+
+namespace PizzaMauiApp.RabbitMq.Messages;
+
+public interface IKitchenOrderAccepted
+{
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public List<IOrderItem> Items { get; set; }
+}
---
+++ IKitchenOrderRejected.cs
@@ -0,0 +1,9 @@
+
+namespace PizzaMauiApp.RabbitMq.Messages;
+
+public interface IKitchenOrderRejected
+{
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public List<IOrderItem> Items { get; set; }
+}
